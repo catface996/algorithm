b26f99d8bc95a0635115e448c37bb98c730d2d54
catface <zlyxzq@163.com>
2021-03-24 17:43:23 +0800

    [新增功能](master): 二叉树前序序列化(反序列化),后续序列化(反序列化),二叉树最大宽度计算方法.

--- src/main/java/pratice/zuo/test/BinaryTree.java
+++ src/main/java/pratice/zuo/test/BinaryTree.java
@@ -1,5 +1,8 @@
 package pratice.zuo.test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author by catface
  * @date 2021/3/22 4:46 下午
@@ -29,6 +32,56 @@ public class BinaryTree<T> {
 
         // 递归打印右孩子
         preOrder(node.rightChild);
+    }
+
+    public void inOrder(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+        // 首先打印左孩子
+        inOrder(node.leftChild);
+
+        // 打印自身节点
+        System.out.print(node.value);
+        System.out.print(";");
+
+        // 最后打印右孩子
+        inOrder(node.rightChild);
+    }
+
+    public void postOrder(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+
+        // 优先打印左孩子
+        postOrder(node.leftChild);
+
+        // 再打印右孩子
+        postOrder(node.rightChild);
+
+        // 最后打印节点本身
+        System.out.print(node.value);
+        System.out.print(";");
+    }
+
+    public void broadTravel(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node<T> tempNode = queue.poll();
+            System.out.print(tempNode.getValue());
+            System.out.print(";");
+            if (tempNode.leftChild != null) {
+                queue.add(tempNode.leftChild);
+            }
+            if (tempNode.rightChild != null) {
+                queue.add(tempNode.rightChild);
+            }
+        }
     }
 
     public static class Node<T> {
--- src/test/java/pratice/zuo/test/BinaryTreeTest.java
+++ src/test/java/pratice/zuo/test/BinaryTreeTest.java
@@ -11,20 +11,54 @@ public class BinaryTreeTest {
 
     @Test
     public void testPreOrder() {
+
         Node<Integer> root = new Node<>();
-        root.setValue(100);
+        root.setValue(0);
         BinaryTree<Integer> bTree = new BinaryTree<>();
-
-        Node<Integer> leftChild = new Node<>();
-        leftChild.setValue(10);
-
-        Node<Integer> rightChild = new Node<>();
-        rightChild.setValue(20);
-
-        root.setLeftChild(leftChild);
-        root.setRightChild(rightChild);
-
         bTree.setRoot(root);
+
+        Node<Integer> l1 = new Node<>();
+        l1.setValue(1);
+
+        Node<Integer> r1 = new Node<>();
+        r1.setValue(2);
+
+        Node<Integer> l11 = new Node<>();
+        l11.setValue(3);
+
+        Node<Integer> l12 = new Node<>();
+        l12.setValue(4);
+
+        Node<Integer> r11 = new Node<>();
+        r11.setValue(5);
+
+        Node<Integer> r12 = new Node<>();
+        r12.setValue(6);
+
+        root.setLeftChild(l1);
+        root.setRightChild(r1);
+
+        l1.setLeftChild(l11);
+        l1.setRightChild(l12);
+
+        r1.setLeftChild(r11);
+        r1.setRightChild(r12);
+
+        System.out.println("前序遍历:");
         bTree.preOrder(bTree.getRoot());
+        System.out.println("");
+
+        System.out.println("中序遍历:");
+        bTree.inOrder(bTree.getRoot());
+        System.out.println("");
+
+        System.out.println("后续遍历:");
+        bTree.postOrder(bTree.getRoot());
+        System.out.println("");
+
+        System.out.println("广度优先遍历:");
+        bTree.broadTravel(bTree.getRoot());
+        System.out.println("");
+
     }
 }
