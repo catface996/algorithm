cbbe9ed6b34cb2761b2cfb028be5f50efe14b187
catface <zlyxzq@163.com>
2021-03-31 16:36:41 +0800

    [新增功能](master): P449序列化和反序列化二叉搜索树

---
+++ src/main/java/pratice/zuo/test/树/P449序列化和反序列化二叉搜索树/Solution2.java
@@ -0,0 +1,95 @@
+package pratice.zuo.test.树.P449序列化和反序列化二叉搜索树;
+
+/**
+ * @author by catface
+ * @date 2021/3/31 3:55 下午
+ */
+public class Solution2 {
+
+    //TODO 4:36 下午	info
+    //				解答成功:
+    //				执行耗时:5 ms,击败了95.78% 的Java用户
+    //				内存消耗:39.2 MB,击败了83.02% 的Java用户
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(1);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        root.right.left.right = new TreeNode(7);
+        Solution2 solution1 = new Solution2();
+        String str = solution1.serialize(root);
+        System.out.println(str);
+        TreeNode root2 = solution1.deserialize(str);
+        System.out.println(root2);
+    }
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        serializeProcess(root, sb);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    private void serializeProcess(TreeNode x, StringBuilder sb) {
+        if (x == null) {
+            return;
+        }
+        sb.append(x.val).append(',');
+        serializeProcess(x.left, sb);
+        serializeProcess(x.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null) {
+            return null;
+        }
+        String[] strArr = data.split(",");
+        int[] values = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            values[i] = Integer.parseInt(strArr[i]);
+        }
+        return deserializeProcess(values, 0, values.length - 1);
+    }
+
+    private TreeNode deserializeProcess(int[] values, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(values[start]);
+        }
+
+        int rootValue = values[start];
+        TreeNode root = new TreeNode(rootValue);
+        int rightStart = start + 1;
+        for (; rightStart <= end; rightStart++) {
+            if (rootValue < values[rightStart]) {
+                break;
+            }
+        }
+        root.left = deserializeProcess(values, start + 1, rightStart - 1);
+        root.right = deserializeProcess(values, rightStart, end);
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+}
--- src/main/java/pratice/zuo/test/树/P99恢复二叉搜索树/Solution.java
+++ src/main/java/pratice/zuo/test/树/P99恢复二叉搜索树/Solution.java
@@ -11,7 +11,96 @@ public class Solution {
     }
 
     private void process(TreeNode x) {
+        if (x == null) {
+            return;
+        }
 
+        TreeNode leftMaxNode = getMaxNode(x.left);
+        TreeNode rightMinNode = getMinNode(x.right);
+        if (leftMaxNode == null && rightMinNode == null) {
+            return;
+        }
+
+        if (leftMaxNode != null && rightMinNode == null) {
+            if (x.val > leftMaxNode.val) {
+                process(x.left);
+            } else {
+
+            }
+        }
+
+        if (leftMaxNode == null && rightMinNode != null) {
+
+        }
+
+        if (leftMaxNode != null && rightMinNode != null) {
+
+        }
+        if (leftMaxNode.val < x.val && x.val < rightMinNode.val) {
+            process(x.left);
+            process(x.right);
+        } else {
+            int max = x.val;
+            max = max > leftMaxNode.val ? max : leftMaxNode.val;
+            max = max > rightMinNode.val ? max : rightMinNode.val;
+
+        }
+
+    }
+
+    private TreeNode getMaxNode(TreeNode x) {
+        if (x == null) {
+            return null;
+        }
+        TreeNode maxNode = x;
+        TreeNode leftMaxNode = getMaxNode(x.left);
+        TreeNode rightMaxNode = getMaxNode(x.right);
+        if (leftMaxNode != null) {
+            maxNode = maxNode.val > leftMaxNode.val ? maxNode : leftMaxNode;
+        }
+        if (rightMaxNode != null) {
+            maxNode = maxNode.val > rightMaxNode.val ? maxNode : rightMaxNode;
+        }
+        return maxNode;
+    }
+
+    private TreeNode getMinNode(TreeNode x) {
+        if (x == null) {
+            return null;
+        }
+        TreeNode minNode = x;
+        TreeNode leftMinNode = getMaxNode(x.left);
+        TreeNode rightMinNode = getMaxNode(x.right);
+        if (leftMinNode != null) {
+            minNode = minNode.val < leftMinNode.val ? minNode : leftMinNode;
+        }
+        if (rightMinNode != null) {
+            minNode = minNode.val < rightMinNode.val ? minNode : rightMinNode;
+        }
+        return minNode;
+    }
+
+    public static class MaxMin {
+
+        private TreeNode minNode;
+
+        private TreeNode maxNode;
+
+        public TreeNode getMinNode() {
+            return minNode;
+        }
+
+        public void setMinNode(TreeNode minNode) {
+            this.minNode = minNode;
+        }
+
+        public TreeNode getMaxNode() {
+            return maxNode;
+        }
+
+        public void setMaxNode(TreeNode maxNode) {
+            this.maxNode = maxNode;
+        }
     }
 
     public class TreeNode {
