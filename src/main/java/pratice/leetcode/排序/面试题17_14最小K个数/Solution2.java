517680b4521ac96f0d4f483bbc29605c52a3e217
catface <zlyxzq@163.com>
2021-04-07 17:44:01 +0800

    [新增功能](master): 面试题17_14最小K个数

---
+++ src/main/java/pratice/leetcode/排序/面试题17_14最小K个数/Solution2.java
@@ -0,0 +1,39 @@
+package pratice.leetcode.排序.面试题17_14最小K个数;
+
+//设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+//
+// 示例：
+//
+// 输入： arr = [1,3,5,7,2,4,6,8], k = 4
+//输出： [1,2,3,4]
+//
+//
+// 提示：
+//
+//
+// 0 <= len(arr) <= 100000
+// 0 <= k <= min(100000, len(arr))
+//
+// Related Topics 堆 排序 分治算法
+// 👍 53 👎 0
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution2 {
+
+    /*
+    使用排序: Arrays.sort 会根据数据规模选择排序策略
+     */
+    public int[] smallestK(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
