87268d28e308b629e70d77c0b83ff48ab57bdfdf
catface <zlyxzq@163.com>
2021-04-08 20:25:14 +0800

    [新增功能](master): 调整打印二叉树的样式.

--- src/main/java/util/PrintTreeNodeUtil.java
+++ src/main/java/util/PrintTreeNodeUtil.java
@@ -9,18 +9,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PrintTreeNodeUtil {
 
-    private static int getMaxDepth(TreeNode x, int depth) {
-        if (x == null) {
-            return depth;
+    /**
+     * 打印二叉树
+     *
+     * @param root 二叉树根节点
+     */
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            System.out.println("空树:[]");
+            return;
         }
-        int leftDepth = getMaxDepth(x.left, depth + 1);
-        int rightDepth = getMaxDepth(x.right, depth + 1);
-        return Math.max(leftDepth, rightDepth);
+
+        char[][] bitmap = initTreeBitmap(root);
+        process(root, 0, bitmap[0].length, 0, bitmap);
+        printSplitLine(bitmap[0].length);
+        System.out.println("节点数:");
+        System.out.println("最大深度:");
+        System.out.println("最大宽度:");
+        System.out.println("叶子节点数:");
+        System.out.println("是否是搜索二叉树(BST):");
+        System.out.println("是否是平衡二叉树(BT)");
+        System.out.println("是否是完全二叉树(CBT):");
+        System.out.println("");
+        printBitmap(bitmap);
     }
 
+    /**
+     * 初始化二叉树位图矩阵
+     *
+     * @param x 树种的某个节点
+     * @return 二维矩阵
+     */
     private static char[][] initTreeBitmap(TreeNode x) {
         int nodeWidth = 2;
-        int depth = getMaxDepth(x, 0);
+        int depth = getMaxDepth(x);
         int row = depth * 2;
         int col = (int)Math.pow(2, depth) * nodeWidth;
         char[][] bitmap = new char[row][col];
@@ -32,21 +54,30 @@ public class PrintTreeNodeUtil {
         return bitmap;
     }
 
-    private static void printBitmap(char[][] bitmap) {
-        for (char[] chars : bitmap) {
-            for (char aChar : chars) {
-                System.out.print(aChar);
-            }
-            System.out.println("");
+    /**
+     * 获得树的最大深度
+     *
+     * @param x 根节点
+     * @return 最大深度
+     */
+    private static int getMaxDepth(TreeNode x) {
+        if (x == null) {
+            return 0;
         }
+        int leftDepth = getMaxDepth(x.left);
+        int rightDepth = getMaxDepth(x.right);
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 
-    public static void printTree(TreeNode root) {
-        char[][] bitmap = initTreeBitmap(root);
-        process(root, 0, bitmap[0].length, 0, bitmap);
-        printBitmap(bitmap);
-    }
-
+    /**
+     * 在二叉树矩阵中,递归设置每个节点的内容
+     *
+     * @param x      待处理节点
+     * @param start  在矩阵中开始书写的起始列
+     * @param end    在矩阵中结束书写的终止列
+     * @param depth  当前处理的深度
+     * @param bitmap 表示二叉树位图的二维矩阵
+     */
     private static void process(TreeNode x, int start, int end, int depth, char[][] bitmap) {
         if (x == null) {
             return;
@@ -72,11 +103,36 @@ public class PrintTreeNodeUtil {
             }
             bitmap[row + 1][sideEnd] = '|';
         }
-        for (int i = 0; i < context.length; i++) {
-            bitmap[row][middle + i - halfContextLength] = context[i];
-        }
+        System.arraycopy(context, 0, bitmap[row], middle - halfContextLength, context.length);
         process(x.left, start, middle, depth + 1, bitmap);
         process(x.right, middle, end, depth + 1, bitmap);
+    }
+
+    /**
+     * 将二叉树的二维矩阵输出到控制台
+     *
+     * @param bitmap 二维矩阵
+     */
+    private static void printBitmap(char[][] bitmap) {
+        for (char[] chars : bitmap) {
+            for (char aChar : chars) {
+                System.out.print(aChar);
+            }
+            System.out.println("");
+        }
+    }
+
+    /**
+     * 打印分割线
+     *
+     * @param length 分割线长度
+     */
+    private static void printSplitLine(int length) {
+        length = Math.max(length, 50);
+        for (int i = 0; i < length; i++) {
+            System.out.print('*');
+        }
+        System.out.println("");
     }
 
 }
