a3df3bdd066d93d0fcdb9e0a8f323c9ee6a857d1
catface <zlyxzq@163.com>
2021-04-20 17:16:48 +0800

    [新增功能](master): 图的拓扑序.
    Signed-off-by: catface <zlyxzq@163.com>

---
+++ src/main/java/lintcode/图/P127拓扑排序/Solution.java
@@ -0,0 +1,49 @@
+package lintcode.图.P127拓扑排序;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author by catface
+ * @date 2021/4/20 10:45 上午
+ */
+public class Solution {
+    /**
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+
+        HashMap<DirectedGraphNode, Integer> indegreeMap = new HashMap<>();
+        for (DirectedGraphNode cur : graph) {
+            indegreeMap.put(cur, 0);
+        }
+
+        for (DirectedGraphNode cur : graph) {
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) + 1);
+            }
+        }
+        Queue<DirectedGraphNode> zeroQueue = new LinkedList<>();
+        for (DirectedGraphNode cur : indegreeMap.keySet()) {
+            if (indegreeMap.get(cur) == 0) {
+                zeroQueue.add(cur);
+            }
+        }
+        ArrayList<DirectedGraphNode> ans = new ArrayList<>();
+        while (!zeroQueue.isEmpty()) {
+            DirectedGraphNode cur = zeroQueue.poll();
+            ans.add(cur);
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) - 1);
+                if (indegreeMap.get(next) == 0) {
+                    zeroQueue.offer(next);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
