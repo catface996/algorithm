c1b93e5776992704cc8f4489b0dab98f678bd5b2
catface <zlyxzq@163.com>
2021-04-23 18:03:26 +0800

    [新增功能](master): 最大子序和
    Signed-off-by: catface <zlyxzq@163.com>

---
+++ src/main/java/leetcode/动态规划/P53最大子序和/SolutionUseCache.java
@@ -0,0 +1,119 @@
+package leetcode.动态规划.P53最大子序和;
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出：6
+//解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1]
+//输出：1
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [0]
+//输出：0
+//
+//
+// 示例 4：
+//
+//
+//输入：nums = [-1]
+//输出：-1
+//
+//
+// 示例 5：
+//
+//
+//输入：nums = [-100000]
+//输出：-100000
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 3 * 104
+// -105 <= nums[i] <= 105
+//
+//
+//
+//
+// 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+// Related Topics 数组 分治算法 动态规划
+// 👍 3155 👎 0
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionUseCache {
+
+    // 超出空间
+    private int max = Integer.MIN_VALUE;
+
+    public int maxSubArray(int[] nums) {
+        int[][] cache = new int[nums.length][nums.length];
+        for (int[] ints : cache) {
+            Arrays.fill(ints, -1);
+        }
+        int sum = processSum(nums, 0, nums.length - 1, cache);
+        return Math.max(max, sum);
+    }
+
+    private int processSum(int[] nums, int left, int right, int[][] cache) {
+        if (left == right) {
+            return nums[left];
+        }
+        int ans = cache[left][right];
+        if (ans != -1) {
+            return ans;
+        }
+        // 除去左侧
+        int noLeft = processSum(nums, left + 1, right, cache);
+        int hasLeft = noLeft + nums[left];
+        max = Math.max(max, noLeft);
+        max = Math.max(max, hasLeft);
+
+        // 除去右侧
+        int notRight = processSum(nums, left, right - 1, cache);
+        max = Math.max(max, notRight);
+        cache[left][right] = hasLeft;
+        return hasLeft;
+    }
+
+    public static class TestClass {
+
+        //输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+        //输出：6
+        //解释：连续子数组[4,-1,2,1] 的和最大，为6 。
+        @Test
+        public void test1() {
+            int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+            SolutionUseCache solution = new SolutionUseCache();
+            int ans = solution.maxSubArray(nums);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test2() {
+            int[] nums = {1};
+            SolutionUseCache solution = new SolutionUseCache();
+            int ans = solution.maxSubArray(nums);
+            System.out.println(ans);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
