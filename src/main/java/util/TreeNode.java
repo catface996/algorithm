f37144b106836fa90994ab7328a77a9d60be18b6
catface <zlyxzq@163.com>
2021-05-06 11:47:40 +0800

    [新增功能](master): 工具类调整.
    Signed-off-by: catface <zlyxzq@163.com>

--- src/main/java/util/TreeNode.java
+++ src/main/java/util/TreeNode.java
@@ -11,9 +11,9 @@ public class TreeNode {
 
     TreeNode() {}
 
-    TreeNode(int val) { this.val = val; }
+    public TreeNode(int val) { this.val = val; }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
