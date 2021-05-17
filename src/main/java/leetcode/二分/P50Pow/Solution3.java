0ba95b8f1eb879ff867e8ef71f48a283e12d3fba
catface <zlyxzq@163.com>
2021-05-17 17:57:09 +0800

    [新增功能](master): 快速幂积
    Signed-off-by: catface <zlyxzq@163.com>

---
+++ src/main/java/leetcode/二分/P50Pow/Solution3.java
@@ -0,0 +1,190 @@
+package leetcode.二分.P50Pow;
+
+//实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：x = 2.00000, n = 10
+//输出：1024.00000
+//
+//
+// 示例 2：
+//
+//
+//输入：x = 2.10000, n = 3
+//输出：9.26100
+//
+//
+// 示例 3：
+//
+//
+//输入：x = 2.00000, n = -2
+//输出：0.25000
+//解释：2-2 = 1/22 = 1/4 = 0.25
+//
+//
+//
+//
+// 提示：
+//
+//
+// -100.0 < x < 100.0
+// -231 <= n <= 231-1
+// -104 <= xn <= 104
+//
+// Related Topics 数学 二分查找
+// 👍 645 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution3 {
+
+    // 5:23 下午	info
+    //				解答失败:
+    //				测试用例:2.00000
+    //				-2147483648
+    //				测试结果:1.00000
+    //				期望结果:0.0
+    public double myPow(double x, int n) {
+        if (Double.valueOf(x).equals(Double.valueOf(0.0))) {
+            return 0;
+        }
+        if (Double.valueOf(x).equals(Double.valueOf(1))) {
+            return 1;
+        }
+        if (n < 0) {
+            n = Math.abs(n);
+            x = 1 / x;
+        }
+        return process(x, n);
+    }
+
+    public double process(double x, int n) {
+        List<Integer> bitList = new ArrayList<>();
+        while (n != 0) {
+            int lowBit = n & -n;
+            bitList.add(lowBit);
+            n -= lowBit;
+        }
+        int times = 1;
+        double p = x;
+        double ans = 1;
+        for (Integer lowBit : bitList) {
+            while (lowBit > times) {
+                times *= 2;
+                p *= p;
+            }
+            ans *= p;
+        }
+        return ans;
+    }
+
+    public static class TestClass {
+
+        @Test
+        public void test1() {
+            Solution3 solution2 = new Solution3();
+            int n = 10;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test2() {
+            Solution3 solution2 = new Solution3();
+            int n = -1;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test3() {
+            Solution3 solution2 = new Solution3();
+            int n = -2;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test4() {
+            Solution3 solution2 = new Solution3();
+            int n = -3;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test5() {
+            Solution3 solution2 = new Solution3();
+            int n = 0;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test6() {
+            Solution3 solution2 = new Solution3();
+            int n = 1;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test7() {
+            Solution3 solution2 = new Solution3();
+            int n = 2;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test8() {
+            Solution3 solution2 = new Solution3();
+            int n = 3;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        @Test
+        public void test9() {
+            Solution3 solution2 = new Solution3();
+            int n = 2147483647;
+            double x = 1.000000001;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+        // 5:23 下午	info
+        //				解答失败:
+        //				测试用例:2.00000
+        //				-2147483648
+        //				测试结果:1.00000
+        //				期望结果:0.0
+        @Test
+        public void test10() {
+            Solution3 solution2 = new Solution3();
+            int n = -2147483648;
+            double x = 2.0;
+            double ans = solution2.myPow(x, n);
+            System.out.println(ans);
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
