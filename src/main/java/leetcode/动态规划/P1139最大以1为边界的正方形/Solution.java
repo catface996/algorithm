781b057dbd52c815a1ac95a1760e2cea0c4c905e
catface <zlyxzq@163.com>
2021-05-26 10:58:03 +0800

    [新增功能](master): 最大以1为边的正方形,各种常数项优化.
    Signed-off-by: catface <zlyxzq@163.com>

---
+++ src/main/java/leetcode/动态规划/P1139最大以1为边界的正方形/Solution.java
@@ -0,0 +1,177 @@
+package leetcode.动态规划.P1139最大以1为边界的正方形;
+
+//给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大 正方形 子网格，并返回该子网格中的元素数量。如果不存在，则返回 0
+//。
+//
+//
+//
+// 示例 1：
+//
+// 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+//输出：9
+//
+//
+// 示例 2：
+//
+// 输入：grid = [[1,1,0,0]]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= grid.length <= 100
+// 1 <= grid[0].length <= 100
+// grid[i][j] 为 0 或 1
+//
+// Related Topics 动态规划
+// 👍 71 👎 0
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    // 7:00 下午	info
+    //				解答成功:
+    //				执行耗时:22 ms,击败了7.19% 的Java用户
+    //				内存消耗:39.1 MB,击败了77.40% 的Java用户
+
+    int[][] sum1ForRow;
+    int[][] sum1ForCol;
+    int[][] grid;
+    int rows;
+    int cols;
+
+    public int largest1BorderedSquare(int[][] grid) {
+        rows = grid.length;
+        cols = grid[0].length;
+        this.grid = grid;
+        sum1ForRow = count1EveryRow(grid);
+        sum1ForCol = count1EveryCol(grid);
+        int ans = 0;
+        for (int startRow = 0; startRow < rows; startRow++) {
+            for (int startCol = 0; startCol < cols; startCol++) {
+                for (int borderLength = ans + 1; borderLength <= getMaxBorderLength(startRow, startCol);
+                     borderLength++) {
+                    if (isSquare(startRow, startCol, borderLength)) {
+                        ans = Math.max(ans, borderLength);
+                    }
+                }
+            }
+        }
+        return ans * ans;
+    }
+
+    private int[][] count1EveryRow(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] sum1ForRow = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            sum1ForRow[row][0] = grid[row][0];
+            for (int col = 1; col < cols; col++) {
+                sum1ForRow[row][col] = sum1ForRow[row][col - 1] + grid[row][col];
+            }
+        }
+        return sum1ForRow;
+    }
+
+    private int[][] count1EveryCol(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] sum1ForCol = new int[rows][cols];
+        for (int col = 0; col < cols; col++) {
+            sum1ForCol[0][col] = grid[0][col];
+            for (int row = 1; row < rows; row++) {
+                sum1ForCol[row][col] = sum1ForCol[row - 1][col] + grid[row][col];
+            }
+        }
+        return sum1ForCol;
+    }
+
+    private boolean isSquare(int startRow, int startCol, int borderLength) {
+        int endRow = startRow + borderLength - 1;
+        int endCol = startCol + borderLength - 1;
+        int right1Num = sum1ForRow[startRow][endCol] - sum1ForRow[startRow][startCol] + grid[startRow][startCol];
+        if (right1Num != borderLength) {
+            return false;
+        }
+        int down1Num = sum1ForCol[endRow][startCol] - sum1ForCol[startRow][startCol] + grid[startRow][startCol];
+        if (down1Num != borderLength) {
+            return false;
+        }
+        int rightDown = sum1ForCol[endRow][endCol] - sum1ForCol[startRow][endCol] + grid[startRow][endCol];
+        if (rightDown != borderLength) {
+            return false;
+        }
+        int downRight = sum1ForRow[endRow][endCol] - sum1ForRow[endRow][startCol] + grid[endRow][startCol];
+        if (downRight != borderLength) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 从起点开始,可能扩展的最大边长
+     *
+     * @param startRow
+     * @param startCol
+     * @return
+     */
+    public int getMaxBorderLength(int startRow, int startCol) {
+        int endRow = rows - 1;
+        int endCol = cols - 1;
+        int right1Num = sum1ForRow[startRow][endCol] - sum1ForRow[startRow][startCol] + grid[startRow][startCol];
+        int down1Num = sum1ForCol[endRow][startCol] - sum1ForCol[startRow][startCol] + grid[startRow][startCol];
+        return Math.min(right1Num, down1Num);
+    }
+
+    public static class TestClass {
+
+        // 示例 1：
+        //
+        // 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+        //输出：9
+        @Test
+        public void test() {
+            int[][] grid = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+            };
+            Solution solution = new Solution();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+
+        // 示例 2：
+        //
+        // 输入：grid = [[1,1,0,0]]
+        //输出：1
+        @Test
+        public void test2() {
+            int[][] grid = {{1, 1, 0, 0}};
+            Solution solution = new Solution();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+
+        // 6:05 下午	info
+        //				解答失败:
+        //				测试用例:[[0]]
+        //				测试结果:1
+        //				期望结果:0
+        //				stdout:
+        @Test
+        public void test3() {
+            int[][] grid = {{0}};
+            Solution solution = new Solution();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/main/java/leetcode/动态规划/P1139最大以1为边界的正方形/Solution4.java
@@ -0,0 +1,157 @@
+package leetcode.动态规划.P1139最大以1为边界的正方形;
+
+//给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大 正方形 子网格，并返回该子网格中的元素数量。如果不存在，则返回 0
+//。
+//
+//
+//
+// 示例 1：
+//
+// 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+//输出：9
+//
+//
+// 示例 2：
+//
+// 输入：grid = [[1,1,0,0]]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= grid.length <= 100
+// 1 <= grid[0].length <= 100
+// grid[i][j] 为 0 或 1
+//
+// Related Topics 动态规划
+// 👍 71 👎 0
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution4 {
+
+    // 10:55 上午	info
+    //					解答成功:
+    //					执行耗时:9 ms,击败了42.32% 的Java用户
+    //					内存消耗:39.1 MB,击败了64.50% 的Java用户
+
+    int[][] serial1InRow;
+    int[][] serial1InCol;
+    int[][] grid;
+    int R;
+    int C;
+
+    public int largest1BorderedSquare(int[][] grid) {
+        this.grid = grid;
+        R = grid.length;
+        C = grid[0].length;
+        countSerial1();
+        // 最外层循环用size来限定
+        int ans = 0;
+        for (int borderLength = Math.min(R, C); borderLength >= 1; borderLength--) {
+            if (hasSquire(borderLength)) {
+                ans = borderLength;
+                break;
+            }
+        }
+        return ans * ans;
+    }
+
+    private boolean hasSquire(int borderLength) {
+        for (int startRow = 0; startRow <= R - borderLength; startRow++) {
+            for (int startCol = 0; startCol <= C - borderLength; startCol++) {
+                if (isSquare(startRow, startCol, borderLength)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void countSerial1() {
+        serial1InRow = new int[R][C];
+        serial1InCol = new int[R][C];
+        for (int row = 0; row < R; row++) {
+            serial1InRow[row][C - 1] = grid[row][C - 1];
+        }
+        for (int col = 0; col < C; col++) {
+            serial1InCol[R - 1][col] = grid[R - 1][col];
+        }
+        for (int col = C - 2; col >= 0; col--) {
+            if (grid[R - 1][col] == 1) {
+                serial1InRow[R - 1][col] = serial1InRow[R - 1][col + 1] + 1;
+            }
+        }
+        for (int row = R - 2; row >= 0; row--) {
+            if (grid[row][C - 1] == 1) {
+                serial1InCol[row][C - 1] = serial1InCol[row + 1][C - 1] + 1;
+            }
+        }
+        for (int row = R - 2; row >= 0; row--) {
+            for (int col = C - 2; col >= 0; col--) {
+                if (grid[row][col] == 1) {
+                    serial1InRow[row][col] = serial1InRow[row][col + 1] + 1;
+                    serial1InCol[row][col] = serial1InCol[row + 1][col] + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isSquare(int startRow, int startCol, int borderLength) {
+        int endRow = startRow + borderLength - 1;
+        int endCol = startCol + borderLength - 1;
+        return serial1InRow[startRow][startCol] >= borderLength && serial1InCol[startRow][startCol] >= borderLength
+            && serial1InCol[startRow][endCol] >= borderLength && serial1InRow[endRow][startCol] >= borderLength;
+    }
+
+    public static class TestClass {
+
+        // 示例 1：
+        //
+        // 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+        //输出：9
+        @Test
+        public void test() {
+            int[][] grid = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+            };
+            Solution4 solution = new Solution4();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+
+        // 示例 2：
+        //
+        // 输入：grid = [[1,1,0,0]]
+        //输出：1
+        @Test
+        public void test2() {
+            int[][] grid = {{1, 1, 0, 0}};
+            Solution4 solution = new Solution4();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+
+        // 6:05 下午	info
+        //				解答失败:
+        //				测试用例:[[0]]
+        //				测试结果:1
+        //				期望结果:0
+        //				stdout:
+        @Test
+        public void test3() {
+            int[][] grid = {{0}};
+            Solution4 solution = new Solution4();
+            int largest = solution.largest1BorderedSquare(grid);
+            System.out.println(largest);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
