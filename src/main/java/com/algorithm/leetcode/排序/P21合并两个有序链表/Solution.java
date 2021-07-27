363f2ca2b6a6148f31df712447b34d198216d4de
catface <zlyxzq@163.com>
2021-07-27 20:53:17 +0800

    [新增功能](master): 合并两个有序链表.
    Signed-off-by: catface <zlyxzq@163.com>

---
+++ src/main/java/com/algorithm/leetcode/排序/P21合并两个有序链表/Solution.java
@@ -0,0 +1,66 @@
+package com.algorithm.leetcode.排序.P21合并两个有序链表;
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+//
+//
+// 示例 2：
+//
+//
+//输入：l1 = [], l2 = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+// Related Topics 递归 链表
+// 👍 1817 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {} ListNode(int val) {
+ * this.val = val; } ListNode(int val, ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) { this.val = val; }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
